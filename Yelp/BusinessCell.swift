836038ceca82ed7462de1e4ff4c46cbca5895deb
Patwardhan, Saurabh <Saurabh.Patwardhan@capitalone.com>
2016-10-18 23:31:11 -0700

    Video 2. Name doesnt wrap though.

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,52 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Patwardhan, Saurabh on 10/18/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    var business : Business! {
+        didSet{
+            nameLabel.text = business.name
+            thumbImageView.setImageWith(business.imageURL!)
+            categoriesLabel.text = business.categories
+            addressLabel.text = business.address
+            reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+            
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
