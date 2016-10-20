b1a33f8495fceed2db7293aa38cc83419c653a32
Patwardhan, Saurabh <Saurabh.Patwardhan@capitalone.com>
2016-10-20 10:28:04 -0700

    New keys

--- Yelp/YelpClient.swift
+++ Yelp/YelpClient.swift
@@ -12,10 +12,10 @@ import AFNetworking
 import BDBOAuth1Manager
 
 // You can register for Yelp API keys here: http://www.yelp.com/developers/manage_api_keys
-let yelpConsumerKey = "vxKwwcR_NMQ7WaEiQBK_CA"
-let yelpConsumerSecret = "33QCvh5bIF5jIHR5klQr7RtBDhQ"
-let yelpToken = "uRcRswHFYa1VkDrGV6LAW2F8clGh5JHV"
-let yelpTokenSecret = "mqtKIxMIR4iBtBPZCmCLEb-Dz3Y"
+let yelpConsumerKey = "hdJF82OEvfIFf4twNecbkg"
+let yelpConsumerSecret = "5mEk9OM_McnVFW7eYN79wxfq2Oo"
+let yelpToken = "Yal62a48U-3tz-pTcpiptfgtrKPc0ED8"
+let yelpTokenSecret = "ZToW-ThHrcKNejfwl6wbp_3YvJc"
 
 enum YelpSortMode: Int {
     case bestMatched = 0, distance, highestRated
